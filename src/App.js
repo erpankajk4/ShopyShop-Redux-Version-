7a0ade98a16a0e1139ae03a6145cd4961385d91e
pankaj <er.pankaj,k4@gmail.com>
2023-08-25 18:06:34 +0530

    setting up TailwindCss

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 function App() {
   return (
-    <div>
-      
-    </div>
+<>
+<h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    
+</>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Include all JavaScript and TypeScript files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
